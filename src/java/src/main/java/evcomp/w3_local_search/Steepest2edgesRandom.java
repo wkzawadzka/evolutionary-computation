c8483554135441603a549d12f6cb41bebab4ec67
wkzawadzka <vee.zawadzka@gmail.com>
2024-11-17 10:18:54 +0100

    (week3) fix delta and initial solution

--- src/java/src/main/java/evcomp/w3_local_search/Steepest2edgesRandom.java
+++ src/java/src/main/java/evcomp/w3_local_search/Steepest2edgesRandom.java
@@ -12,6 +12,8 @@ import java.util.Random;
 
 public class Steepest2edgesRandom {
     private static Random RANDOM;
+    private static Evaluator evaluator;
+
     public static void main(String[] args) {
         // Args
         if (args.length != 2) {
@@ -27,23 +29,30 @@ public class Steepest2edgesRandom {
             return;
         }
 
-        // Initialize the Random instance with the seed
-        RANDOM = new Random(222);
-
         // Initialize InputGenerator with given instance
         InputGenerator inputGenerator = new InputGenerator(instance);
+        // Initialize evaluator
+        evaluator = new Evaluator();
         // Retrieve the distance matrix and node list from InputGenerator
         int[][] distanceMatrix = inputGenerator.getDistanceMatrix();
         List<Node> nodeList = inputGenerator.getNodeList();
 
+        RANDOM = new Random(222);
+
+        // Create a list of node IDs to choose from
+        List<Integer> ids = new ArrayList<>();
+        for (Node node : nodeList) {
+            ids.add(node.getId());
+        }
+
         SolutionSaver solutionSaver = new SolutionSaver("w3_local_search", "Steepest2edgesRandom", instance);
-        Evaluator evaluator = new Evaluator();
 
         // Perform experiment
         for (int i = 1; i <= nExperiments; i++) {
+            List<Integer> initialSolution = generateInitialRandomSolution(ids, nodeList, distanceMatrix);
             long startTime = System.currentTimeMillis();
 
-            List<Integer> randomSolution = generateSteepest2edgesRandom(nodeList, distanceMatrix, evaluator);
+            List<Integer> randomSolution = generateSteepest2edgesRandom(initialSolution, nodeList, distanceMatrix);
 
             long timeTaken = System.currentTimeMillis() - startTime;
 
@@ -55,27 +64,43 @@ public class Steepest2edgesRandom {
         }
 
     }
-    private static List<Integer> generateSteepest2edgesRandom(List<Node> nodeList, int[][] distanceMatrix, Evaluator eval) {
-        List<Integer> selectedIds = new ArrayList<>();
+
+    private static List<Integer> generateInitialRandomSolution(List<Integer> ids, List<Node> nodeList, int[][] distanceMatrix) {
+        List<Integer> bestSolution = null;
+        int lowestObjectiveValue = Integer.MAX_VALUE;
+
         int totalNodes = nodeList.size();
         int numberToSelect = totalNodes / 2; // Select 50%
 
-        // Create a list of node IDs to choose from
-        List<Integer> ids = new ArrayList<>();
-        for (Node node : nodeList) {
-            ids.add(node.getId());
+        // 200 randomly generated solutions
+        for (int i = 0; i < 200; i++) {
+            Collections.shuffle(ids, RANDOM);
+
+            List<Integer> selectedIds = new ArrayList<>();
+            for (int j = 0; j < numberToSelect; j++) {
+                selectedIds.add(ids.get(j));
+            }
+
+            // Evaluate the objective function
+            int totalCost = evaluator.calculateTotalCost(selectedIds, nodeList);
+            int totalDistance = evaluator.calculateTotalDistance(selectedIds, distanceMatrix);
+            int objFuncValue = evaluator.calculateObjectiveFunction(totalCost, totalDistance);
+
+            if (objFuncValue < lowestObjectiveValue) {
+                lowestObjectiveValue = objFuncValue;
+                bestSolution = new ArrayList<>(selectedIds); 
+            }
         }
 
-        // Shuffle the IDs using the seeded random instance
-        // Random initial solution:
-        java.util.Collections.shuffle(ids, RANDOM);
-        for (int i = 0; i < numberToSelect; i++) {
-            selectedIds.add(ids.get(i));
-        }
+        return bestSolution;
+    }
 
-        int selectedCost = eval.calculateTotalCost(selectedIds, nodeList) + eval.calculateTotalDistance(selectedIds, distanceMatrix);
-        // System.out.println(selectedIds);
-        // System.out.println(selectedCost);
+    private static List<Integer> generateSteepest2edgesRandom(List<Integer> initialSolution, List<Node> nodeList, int[][] distanceMatrix) {
+        int totalNodes = nodeList.size();
+        int numberToSelect = totalNodes / 2; // Select 50%
+
+        List<Integer> selectedIds = initialSolution;
+        int selectedCost = evaluator.calculateTotalCost(selectedIds, nodeList) + evaluator.calculateTotalDistance(selectedIds, distanceMatrix);
 
         boolean go = true;
         while(go){
@@ -84,36 +109,62 @@ public class Steepest2edgesRandom {
             for (int i = 0; i < numberToSelect; i++){
                 for (int j = 0; j < totalNodes; j++){
                     int idj = nodeList.get(j).getId();
+                    int delta = 0;
                     List<Integer> swapped = new ArrayList<>(selectedIds);
                     if (!selectedIds.contains(idj)){
+                        int prevNode1 = (i > 0) ? selectedIds.get(i - 1) : selectedIds.get(numberToSelect-1);
+                        int nextNode1 = (i < selectedIds.size() - 1) ? selectedIds.get(i + 1) : selectedIds.get(0);
+        
                         swapped.set(i, idj);
+        
+                        int oldCost = distanceMatrix[prevNode1][selectedIds.get(i)]
+                                    + distanceMatrix[selectedIds.get(i)][nextNode1] 
+                                    + nodeList.get(selectedIds.get(i)).getCost();
+        
+                        int newCost = distanceMatrix[prevNode1][idj]
+                                    + distanceMatrix[idj][nextNode1]
+                                    + nodeList.get(idj).getCost();
+        
+                        delta =  newCost - oldCost;
                     }
                     else{ // intra - 2 edges
                         int indexIdj = selectedIds.indexOf(idj);
-                        // if next to each other - no
-                        if (Math.abs(i - indexIdj) == 1) {
-                            continue; 
-                        }
                         int start_ = Math.min(i, indexIdj);
                         int end = Math.max(i, indexIdj);
+
+                        // if next to each other - no
+                        if (Math.abs(i - indexIdj) <= 1 || (start_ == 0 && end == numberToSelect - 1)) {
+                            continue;
+                        }
                         
+                        // before swapping, get the nodes connected to the two edges (before swap)
+                        int prevNode1 = selectedIds.get((start_ - 1 + numberToSelect) % numberToSelect);  // the node before the start of the first edge
+                        int nextNode1 =  selectedIds.get((end + 1) % numberToSelect);  // the node after the end of the second edge
+
+
                         List<Integer> sublist = swapped.subList(start_, end + 1);
                         Collections.reverse(sublist);
+
+                        int oldDistance = distanceMatrix[prevNode1][selectedIds.get(start_)] + distanceMatrix[selectedIds.get(end)][nextNode1];
+                        int newDistance = distanceMatrix[prevNode1][selectedIds.get(end)] + distanceMatrix[selectedIds.get(start_)][nextNode1];
+
+                        delta = newDistance - oldDistance;
                     }
 
-                    int change = eval.calculateTotalCost(swapped, nodeList) + eval.calculateTotalDistance(swapped, distanceMatrix);
-                    if (change<bestchange){
-                        bestchange = change;
+                    if (delta<bestchange){
+                        //System.out.println("[i = " + i + "] Curr selected cost: " + selectedCost);
+                        //System.out.println("Curr delta: " + delta);
+                        //System.out.println("Curr bestchange: " + bestchange);
+                        bestchange = delta;
                         bestsol = swapped;
                     }
                 }
             }
-            //System.out.println(bestchange);
 
             // Steepest:
-            if (bestchange<selectedCost){
+            if (selectedCost+bestchange<selectedCost){
                 selectedIds = bestsol;
-                selectedCost = bestchange;
+                selectedCost = selectedCost+bestchange;
             }
             else {
                 go = false;
